29523972746d7415c135ff5aa1e6868cf4407cd0
kmat100 <kmat100@naver.com>
2021-09-23 15:04:31 +0900

    feat : git1

---
+++ src/test1git.java
@@ -0,0 +1,18 @@
+public class test1git {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        int add = a+b;
+        int sub = a-c;
+        int div = c/b;
+        int mult = c*b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(div);
+        System.out.println(mult);
+    }
+}
